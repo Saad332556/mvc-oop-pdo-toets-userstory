55481ef3ad62a4f102040548391aa8ca92c94d93
Saad332556 <332556@student.mboutrecht.nl>
2023-01-12 11:22:47 +0100

    Initial commit

---
+++ app/config/config.php
@@ -0,0 +1,11 @@
+<?php
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'Mvc-2019a');
+
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    define('URLROOT', 'http://www.mvc-oop-pdo-toets-userstory.com');
+
+    define('SITENAME', 'mvc-oop-pdo-toets-userstory');
